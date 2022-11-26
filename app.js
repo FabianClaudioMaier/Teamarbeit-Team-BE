b5c9b7ebb331b6dfd6735c91a158f2eb403f713d
Fabian <fabian.maier@stud.fh-campuswien.ac.at>
2022-11-26 17:01:19 +0100

    commited app.js with new Object Cell Array

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 let ctx   = canvas.getContext("2d");
-var cells = [[1,0],[0,1],[1,2],[3,1]];
+function Cell(x,y){
+    this.x = x;
+    this.y = y;
+}
+
+
+var cells = [
+    cell = new Cell(0,1),
+    cell = new Cell(1,0),
+    cell = new Cell(1,2),
+    cell = new Cell(3,1)
+];
+
+
+console.log(cells);
+
 //var cells = [];
-var grid = [];
+//var grid = [];
 var colums = 4;
 var rows = colums;
 
@@ -11,7 +26,7 @@ var rows = colums;
 setInterval(() => { 
     ctx.clearRect(0,0,256,256); 
     ctx.fillStyle = "white";
-    cells.forEach(([x,y]) => ctx.fillRect(x * (256/rows), y * (256/colums), 256/rows, 256/colums)); 
+    cells.forEach(Cell => ctx.fillRect(cell.x * (256/rows), cell.y * (256/colums), 256/rows, 256/colums)); 
     console.log(cells);
     updateCells();
     console.log(cells);
@@ -23,7 +38,7 @@ function fillCells(){
 for (let i = 0; i < 6; i++) {
 var x = Math.floor(Math.random()*colums);
 var y = Math.floor(Math.random()*rows);
-if(cells.filter(([i,j]) => i == x && j == y).length == 0){ cells.push([x,y])};
+if(cells.filter(cell => cell.x == x && cell.y == y) == 0){ cells.push(new Cell(x,y))};
 };
 }
 
@@ -35,27 +50,27 @@ for (let x = 0; x < colums; x++) {
         //look for neighborgs
         for (let i = (x-1 < 0? 0 : x-1); i <= x+1 && i < colums; i++) {
             for (let j = (y-1 < 0? 0 : y-1); j <= y+1 && j < rows; j++) {
-                if(save.filter(([x,y]) => x == i && y == j).length != 0 && !(i == x && j == y)){ sum++; }       
+                if(save.filter(cell => cell.x == i && cell.y == j && !(cell.x == x && cell.y == y)) != 0 ){ sum++; }       
             };   
         };
         //--sum; to exclude the block itself which was include in the for-loops
         var eins = 1;
 
-        var cellExists = save.filter(([i,j]) => i == x && j == y).length;
+        var cellExists = save.filter(cell => cell.x == x && cell.y == y).length;
 
         if((cellExists == 0 || cellExists == undefined) &&  sum == 3){
-            cells.push([x,y]); //if the [x,y] pair has 3 neighbours and is not alife => get alife
+            cells.push(new Cell(x,y)); //if the [x,y] pair has 3 neighbours and is not alife => get alife
         };
 
         if(cellExists != undefined && (sum <= 1 || sum >= 4)){
             
             var deleteIndex = cells.findIndex(checkCells);
 
-            function checkCells([i,j]) {
-                return i == x && j == y;
+            function checkCells(cell) {
+                return cell.x == x && cell.y == y;
             }
             
-            cells.splice(0, 1) //if the [x,y] pair is alife and has to few or to many neighbours => die
+            cells.splice(deleteIndex, 1) //if the [x,y] pair is alife and has to few or to many neighbours => die
         };
     };
 }
