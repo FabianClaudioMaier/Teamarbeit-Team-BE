1356779177dbda7d465e62b6090bbbce452eb657
Fabian <fabian.maier@stud.fh-campuswien.ac.at>
2023-01-06 18:24:57 +0100

    next Step button added

--- src/main/java/com/example/demo/GameOfLife.java
+++ src/main/java/com/example/demo/GameOfLife.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -28,8 +32,45 @@ public class GameOfLife extends Application {
     public void start(Stage primaryStage) throws IOException {
         Group root = new Group();
         FXMLLoader fxmlLoader = new FXMLLoader(GameOfLife.class.getResource("Style.fxml"));
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        Scene scene = new Scene(root, WIDTH + 100, HEIGHT + 100);
         Scene menu = new Scene(fxmlLoader.load(), 300,200);
+
+        Button nextStep = new Button("next Step");
+        nextStep.setLayoutX(100);
+        nextStep.setLayoutY(HEIGHT + 10);
+
+        /*ToggleButton RunStop = new ToggleButton("Run");
+        RunStop.setLayoutX(200);
+        RunStop.setLayoutY(HEIGHT + 10);*/
+
+        nextStep.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                updateGeneration();
+                for (int row = 0; row < ROWS; row++) {
+                    for (int col = 0; col < COLUMNS; col++) {
+                        ((Rectangle) root.getChildren().get(row * COLUMNS + col)).setFill(currentGeneration[row][col] ? Color.BLACK : Color.WHITE);
+                    }
+                }
+            }
+        });
+
+       /* RunStop.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(RunStop.)
+                updateGeneration();
+                for (int row = 0; row < ROWS; row++) {
+                    for (int col = 0; col < COLUMNS; col++) {
+                        ((Rectangle) root.getChildren().get(row * COLUMNS + col)).setFill(currentGeneration[row][col] ? Color.BLACK : Color.WHITE);
+                    }
+                }
+            }
+        });*/
+
+
 
         initializeCurrentGeneration();
 
@@ -41,10 +82,12 @@ public class GameOfLife extends Application {
             }
         }
 
+        root.getChildren().add(nextStep);
+
         primaryStage.setScene(scene);
         primaryStage.show();
-        primaryStage.setScene(menu);
-        primaryStage.show();
+        //primaryStage.setScene(menu);
+        //primaryStage.show();
 
         scene.setOnMouseClicked(event -> {
             int row = (int) (event.getY() / CELL_SIZE);
