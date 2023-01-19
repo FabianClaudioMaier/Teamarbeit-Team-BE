1255fd6f0fe0107d113413a48c888a7bd4e7c900
Laurin Knünz <laurin.knuenz@stud.fh-campuswien.ac.at>
2023-01-19 18:09:16 +0100

    Fixed ArrayGameHandler and added Coordinates Models Class

--- src/main/java/com/example/demo/ArrayGameHandler.java
+++ src/main/java/com/example/demo/ArrayGameHandler.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.Models.Coordinates;
+
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ArrayGameHandler {
     private final Array array;
     private boolean gameStatePlay = false;
-    private final Timer timer = new Timer();
+    private Timer timer;
+
     private final TimerTask ttUpdateArray = new TimerTask() {
         @Override
         public void run() {
@@ -20,8 +24,29 @@ public class ArrayGameHandler {
     }
 
     public void playPause() {
-        if (gameStatePlay) timer.cancel();
-        else timer.scheduleAtFixedRate(ttUpdateArray, 0, 500);
+        if (gameStatePlay) {
+            timer.cancel();
+            timer = null;
+        } else {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(ttUpdateArray, 0, 500);
+        }
         gameStatePlay = !gameStatePlay;
+    }
+
+    public void nextStep() {
+        array.update();
+        System.out.println(array);
+    }
+
+    public void changeCellStatus(int row, int col) {
+        array.setArray(row, col);
+        System.out.println(array);
+    }
+
+    public void changeHoverCellsStatus(List<Coordinates> listOfCoordinates) {
+        for (Coordinates coordinate : listOfCoordinates) {
+            changeCellStatus(coordinate.getxCoordinate(), coordinate.getyCoordinate());
+        }
     }
 }
---
+++ src/main/java/com/example/demo/Models/Coordinates.java
@@ -0,0 +1,19 @@
+package com.example.demo.Models;
+
+public class Coordinates {
+    private final int xCoordinate;
+    private final int yCoordinate;
+
+    public Coordinates(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+}
